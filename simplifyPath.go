d7afe86e97699f60c2efdc39adf344e2e5777cd5
archer <archer@thenetcircle.com>
2020-04-19 11:51:27 +0000

    simlifypath

---
+++ simplifyPath.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+import "os"
+import "strings"
+
+func main() {
+	path := "/a//b////c/d//././/.."
+	// path = "/.../"
+	// path = "/a/./b/../../c/"
+	path = "/a/../../b/../c//.//"
+	// path = "/..hidden"
+	// path = "/home/foo"
+	// path = "/a//b////c/d//././/.."
+	// path = "/home//foo/"
+	fmt.Println(simplifyPath(path))
+	os.Exit(1)
+}
+
+func simplifyPath(path string) string {
+	pathMap := make(map[int] string)
+	temp := strings.Split(path, "/")
+	i := 0
+	for _, v := range temp {
+		if v != "" && v != ".." && v != "." {
+			pathMap[i] = v
+			i++
+		} else if v == ".." {
+			i--
+			delete(pathMap, i)
+			if i < 0 {
+				i = 0
+			}
+		}
+	}
+
+	result := "/"
+	for j := 0; j < i; j++ {
+		str := pathMap[j]
+		if str != "" {
+			result += str
+		}
+		val, ok := pathMap[j+1]
+		if ok && val != "" {
+			result += "/"
+		}
+	}
+
+	return result    
+}
